5c279353f9b51cad3d70a0d2cea98a505d649f83
describer <tholsclaw@protonmail.com>
2018-07-17 20:40:31 -0700

    update css, created API call in js

--- js/site.js
+++ js/site.js
@@ -1,5 +1,18 @@
-fuction getUE() {
+var request = new XMLHttpRequest();
 
+request.open('GET', 'https://www.quandl.com/api/v3/datasets/FRED/GDPPOT.json?api_key=qfTdSqS1fCHU84LE41Zq', true);
+request.onload = function () {
+
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response);
+
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(date => {
+      console.log(date.value);
+    });
+  } else {
+    console.log('error');
+  }
 }
 
-https://www.quandl.com/api/v3/datasets/FRED/NROUST?column_index=1&api_key=qfTdSqS1fCHU84LE41Zq
+request.send();
