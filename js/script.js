1a0848c3c0084caecc852d0c39050362a17b0aa7
describer <tholsclaw@protonmail.com>
2018-07-18 21:02:47 -0700

    added API call

--- js/script.js
+++ js/script.js
@@ -1,18 +1,29 @@
-var request = new XMLHttpRequest();
-
-request.open('GET', 'https://www.quandl.com/api/v3/datasets/FRED/GDPPOT.json?api_key=qfTdSqS1fCHU84LE41Zq', true);
-request.onload = function () {
-
-  // Begin accessing JSON data here
-  var data = JSON.parse(this.response);
-
-  if (request.status >= 200 && request.status < 400) {
-    data.forEach(date => {
-      console.log(date.value);
-    });
-  } else {
-    console.log('error');
+function httpGet(theUrl) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    let  data = JSON.parse(xmlHttp.responseText)
+    console.log(data.dataset.data);
   }
+
+function todayDate() {
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //January is 0!
+    var yyyy = today.getFullYear();
+
+    if(dd<10) {
+      dd = '0'+dd
+    }
+
+    if(mm<10) {
+      mm = '0'+mm
+    }
+
+    today = yyyy + '-' + mm + '-' + dd;
+
+    console.log(today);
+    httpGet("https://www.quandl.com/api/v3/datasets/FRED/GDPPOT.json?api_key=qfTdSqS1fCHU84LE41Zq&collapse=annual&end_date=" + today);
 }
 
-request.send();
+todayDate()
